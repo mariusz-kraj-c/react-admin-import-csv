27edd4624a9b4a5032e93e0cb0237b5bcc1b3259
Mariusz Kraj <mariusz.kraj@codibly.com>
2023-02-21 13:03:49 +0100

    built lib

---
+++ lib/config.interface.d.ts
@@ -0,0 +1,18 @@
+import { ParseConfig } from "papaparse";
+export interface ImportConfig {
+    logging?: boolean;
+    disableCreateMany?: boolean;
+    disableUpdateMany?: boolean;
+    disableGetMany?: boolean;
+    disableImportNew?: boolean;
+    disableImportOverwrite?: boolean;
+    meta?: any[];
+    preCommitCallback?: PrecommitCallback;
+    postCommitCallback?: ErrorCallback;
+    transformRows?: (csvRows: any[]) => Promise<any[]>;
+    validateRow?: ValidateRowFunction;
+    parseConfig?: ParseConfig;
+}
+export declare type PrecommitCallback = (action: "create" | "overwrite", values: any[]) => Promise<any[]>;
+export declare type ValidateRowFunction = (csvRowItem: any, index?: any, allItems?: any[]) => Promise<void>;
+export declare type ErrorCallback = (error: any) => void;
---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { MainCsvImport } from "./main-csv-button";
+import { ImportConfig } from "./config.interface";
+import * as i18n from "./i18n";
+export { MainCsvImport as ImportButton, i18n };
+export type { ImportConfig };
+export default MainCsvImport;
